c029ae47fff4eed331a10224c65c69239f7efd04
Artak Abrahamyan <abrahamyan@outlook.com>
2021-04-29 18:30:00 +0400

    Add project files.

---
+++ Dictionary/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Dictionary
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new()
+            {
+                Id = 101,
+                Name = "Mark",
+                Salary = 5000
+            };
+            Customer customer2 = new()
+            {
+                Id = 102,
+                Name = "Pam",
+                Salary = 6500
+            };
+            Customer customer3 = new()
+            {
+                Id = 103,
+                Name = "John",
+                Salary = 2000
+            };
+
+            Dictionary<int, Customer> customersDictionary = new();
+            customersDictionary.Add(customer1.Id, customer1);
+            customersDictionary.Add(customer2.Id, customer2);
+            customersDictionary.Add(customer3.Id, customer3);
+
+            Customer customer101 = customersDictionary[101];
+
+            foreach (KeyValuePair<int,Customer> customerKeyValuePair in customersDictionary)
+            {
+                Console.WriteLine($"Key - {customerKeyValuePair.Key} Name - { customerKeyValuePair.Value.Name}");
+            } 
+        }
+    }
+}
